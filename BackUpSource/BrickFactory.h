79fed2b4f89980481dcb865050ea2c24aeb5f1a7
PicassoCT <PicassoCT@gmx.de>
2017-07-19 09:57:31 +0200

    Create BrickFactory.h

---
+++ BackUpSource/BrickFactory.h
@@ -0,0 +1,18 @@
+  include "KubeCombination.h"
+  
+  
+   class Brickfactory
+{
+
+    buildBrick(brickType type, KubeCombination* kC);
+
+   
+    void buildLBRICK(Colour choosenColour, KubeCombination* kC);
+    void buildTBRICK(Colour choosenColour, KubeCombination* kC);
+    void buildSBRICK(Colour choosenColour, KubeCombination* kC);
+    void buildZBRICK(Colour choosenColour, KubeCombination* kC);
+    void buildIBRICK(Colour choosenColour, KubeCombination* kC);
+    void buildOBRICK(Colour choosenColour, KubeCombination* kC);
+    
+ }
+    
