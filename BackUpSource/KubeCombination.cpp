12a61513aec4b3a74ede00a0b8961fb1f2347787
PicassoCT <PicassoCT@gmx.de>
2017-07-19 14:04:21 +0200

    Update KubeCombination.cpp

--- BackUpSource/KubeCombination.cpp
+++ BackUpSource/KubeCombination.cpp
@@ -20,12 +20,65 @@ KubeCombination::KubeCombination(vec Pivot,  Colour colOfCube, brickType shapeOf
     bool KubeCombination::doesPlayerCollide(fpsent *d) { return false};
 
   //  vec Pivot;
-    void KubeCombination::updateCubeRotation( Kube* kube);
-    void KubeCombination::upateCubePosition( Kube* kube);
-
+    void KubeCombination::updateCubesRotation( ){
+		for (x=0, x < ARR_ORG_SIZE; x++){
+		for (y=0, y < ARR_ORG_SIZE; y++){
+		for (z=0, z < ARR_ORG_SIZE; z++){
+			if (cubeOrgPositions[x][y][z] != NULL) {
+				cubeOrgPositions[x][y][z].yaw = yaw;
+				cubeOrgPositions[x][y][z].pitch = pitch;
+				cubeOrgPositions[x][y][z].roll = roll;
+				
+			}
+		
+		
+	}
+	
+	
+    void KubeCombination::upateCubesPosition( ){
+		vec localIndex;
+		for (x=0, x < ARR_ORG_SIZE; x++){
+		for (y=0, y < ARR_ORG_SIZE; y++){
+		for (z=0, z < ARR_ORG_SIZE; z++){
+			if (cubeOrgPositions[x][y][z] != NULL) {
+				LOC_INDX(x,y,z)	
+				localIndex *= KUBESIZE;
+				//Rotate around center
+				localIndex = localIndex.rotate_around_x(yaw);
+				localIndex = localIndex.rotate_around_y(pitch);
+				localIndex = localIndex.rotate_around_z(roll);
+				
+				//Add KubeCombinations Position
+				localIndex += o;
+				cubeOrgPositions[x][y][z]->SetPosition(localIndex);
+			}
+		}
+		}
+		}
+		
+		
+	};
+	
+	//retrieves the index of a cub in a zero-centric +- coordinate system, with the pivot at zero
+    vec KubeCombination::getKubeIndex(Kube* kube){
+		int lix,liy,liz;
+		
+		for (x=0, x < ARR_ORG_SIZE; x++){
+		for (y=0, y < ARR_ORG_SIZE; y++){
+		for (z=0, z < ARR_ORG_SIZE; z++){
+				if (cubeOrgPositions[x][y][z] != NULL && cubeOrgPositions[x][y][z] == kube){
+					LOC_INDX(x,y,z)
+					return vec(lix,liy,liz);
+				}
+		}
+		}
+		}
+		return NULL;
+	}
     //TransmitCombinationToCompound
     vector<Kube*>   internalCubes;
 
+	//Adds a Cube - taking the first Cube as Pivot
  void KubeCombination::addCubeAt(vec index, Kube* kubeToAdd) {
 	int ind_x,ind_y,ind_z;
 	
@@ -40,12 +93,13 @@ KubeCombination::KubeCombination(vec Pivot,  Colour colOfCube, brickType shapeOf
 	}
 	else {
 		firstCubeAdded= false;
+		Pivot = index;
 		internalCubes.push_back(kubeToAdd);
 		cubeOrgPositions[INDX_SHFT(ind_x,ARR_ORG_SIZE)][INDX_SHFT(ind_y,ARR_ORG_SIZE)][INDX_SHFT(ind_z,ARR_ORG_SIZE)] = kubeToAdd;
 
 	}
- 		updateCubePosition(kubeToAdd);
-		updateCubeRotation(kubeToAdd);
+ 		upateCubesPosition();
+		updateCubesRotation();
  
  }
 
