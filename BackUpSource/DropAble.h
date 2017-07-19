b7d0ab8fa9fd046dc5473835b870398d1545bdcc
PicassoCT <PicassoCT@gmx.de>
2017-07-19 15:13:44 +0200

    Update DropAble.h

--- BackUpSource/DropAble.h
+++ BackUpSource/DropAble.h
@@ -1,1 +1,45 @@
 
+#ifndef DROPABLE
+#define DROPABLE
+
+#include "KubeCombination.h"
+
+
+namespace game
+{
+
+  class DropAble {
+  public:
+  DropAble();
+  DisolveDropAble();
+  
+  testTurn();
+  startTurn();
+  resetTurn();
+  
+  testMove();
+  startMove();
+  resetMove();
+  
+  animate(float frames);
+  removeLine();
+  
+  private:
+    KubeCombination* kC;
+    vec orgTurnAngleRad;
+    vec targTurnAngleRad;
+    float turnProgress = 0.0;
+
+    vec orgPivotPosition;
+    vec targPivotPosition;
+    float moveProgress = 0.0;
+    bool playerControlled = true;
+
+
+
+
+
+  };
+
+}
+#endif //DROPABLE
